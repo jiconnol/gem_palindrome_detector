f04cfa711e2caa71147eae46c0126edc44b08be4
James I Connolly <jiconnol@gmail.com>
2019-06-01 16:45:29 -0400

    Test Driven Development: Refactoring Exercise

--- lib/jiconnol_palindrome.rb
+++ lib/jiconnol_palindrome.rb
@@ -1,6 +1,27 @@
 require "jiconnol_palindrome/version"
 
-module JiconnolPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
-end
+class String
+
+    # Returns true for a palindrome, false otherwise.
+    def palindrome?
+      processed_content == processed_content.reverse
+    end
+
+  # Returns mixed character content for palindrome testing.
+    def letters
+      the_letters = []
+      for i in 0..self.length - 1 do
+        if self[i].match(/[a-zA-Z]/)
+          the_letters << self[i]
+      end
+    end
+    the_letters.join
+  end
+
+private
+
+    # Returns mixed case content for palindrome testing.
+    def processed_content
+      self.letters.downcase
+    end
+  end
